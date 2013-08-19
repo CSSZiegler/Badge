d91b093a06b28d474390fd3a1f317b791fa35c36
unknown <KH1183@KH1183.kitspl.com>
2013-08-19 20:52:22 +0530

    first commit

---
+++ BadgeAPI/modules/js/appSpecific.js
@@ -0,0 +1,51 @@
+/*****************************************************************
+*	Name    : setBadge
+*	Author  : Kony
+*	Purpose : To set initial badge value as '0' for button widget 'btnBadge' and application icon.
+******************************************************************/	
+
+function setBadge( )
+{
+	frmBadge.btnBadge.setBadge("0");//Set badge value on  button widget
+	if(kony.os.deviceInfo().name == "iPhone" || kony.os.deviceInfo().name == "iPad")
+	{
+		kony.application.setApplicationBadgeValue("0");
+		frmBadge.lblInstruction2.setVisibility(true);
+	}
+}
+
+/*****************************************************************
+*	Name    : BadgeIncrease
+*	Author  : Kony
+*	Purpose : To increase the badge value on button 'btnBadge' as well as application icon when ever user clicks the button 'btnBadge'.
+******************************************************************/
+
+function BadgeIncrease( )
+{
+	var counter = kony.os.toNumber(frmBadge.btnBadge.getBadge ()) + 1;// read badge value from button and increment it with 1 
+	kony.print("this gets executed " + counter+ " type is "+typeof(counter));
+	frmBadge.btnBadge.setBadge(""+counter);// Set badge value on the button widget
+	if(kony.os.deviceInfo().name == "iPhone" || kony.os.deviceInfo().name == "iPad")
+		kony.application.setApplicationBadgeValue(""+counter);//Set badge value on app icon
+	else
+	{
+		frmBadge.lblInstruction2.text = "*Your Current Platform Doesn't support application badge on icon."
+		frmBadge.lblInstruction2.setVisibility(true)
+	}
+}
+
+/*****************************************************************
+*	Name    : setApplicationBadge
+*	Author  : Kony
+*	Purpose : To set application badge using user input
+******************************************************************/
+
+function setApplicationBadge()
+{
+	var w = kony.os.toNumber(frmBadge.tbxSetBadge.text);
+	if(kony.os.deviceInfo().name == "iPhone" || kony.os.deviceInfo().name == "iPad")
+		kony.application.setApplicationBadgeValue(w);
+	else
+		alert("Your current platform doesn't support badge on icon.");
+}
+
---
+++ README.md
@@ -0,0 +1,16 @@
+BadgeAPI
+========
+
+This application will showcase the usage of Badge APIs.
+
+Project Owner: Malla Nandikonda <Malla.Nandikonda@kony.com>
+
+# Purpose
+This Application will showcase the following usage of Badge APIs
+
+* Setting Badge value on a Widget
+* Setting Badge value on Application Icon
+
+# Supported platforms:
+iPhone, iPad, Android, Android tablet
+
